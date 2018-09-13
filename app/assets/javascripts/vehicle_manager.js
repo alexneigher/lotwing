c75174a3b78ffda24ce6154185afab310d9b03ab
Alex Neigher <alexneigher@Alexs-MacBook-Pro.local>
2018-09-13 08:03:33 -0700

    dont need tooltip on vehicle manager

--- app/assets/javascripts/vehicle_manager.js
+++ app/assets/javascripts/vehicle_manager.js
@@ -20,32 +20,6 @@ $(function(){
     }
   });
 
-  var popup = new mapboxgl.Popup({
-      closeButton: false,
-      closeOnClick: false
-  });
-
-  map.on('mouseenter', 'used_vehicle_occupied_spaces', function(e) {
-     
-      var coordinates = e.features[0].geometry.coordinates;
-      var description = "Shape: " + e.features[0].properties.shape_id;
-
-      // Ensure that if the map is zoomed out such that multiple
-      // copies of the feature are visible, the popup appears
-      // over the copy being pointed to.
-      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-      }
-
-      popup.setLngLat(coordinates[0][0])
-        .setHTML(description)
-        .addTo(map);
-  });
-
-  map.on('mouseleave', 'used_vehicle_occupied_spaces', function() {
-      popup.remove();
-  });
-
   window.map.on('load', function () {
 
     fetch_data_and_render('parking_lots');
