0d6098655e1341fc6de9cedf7ef219415a564c7e
Alex Neigher <alexneigher@Alexs-MacBook-Pro.local>
2018-09-27 07:36:39 -0700

    cleaner age on vehicle table

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,6 @@ class HomeController < ApplicationController
 
   def show
     dealership = current_user.dealership
-    
     @users_with_events = current_user.dealership.users.includes(events: :tag).where(tags: {active: true})
   end
 
