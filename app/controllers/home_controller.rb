627eddf8f82e74f11dfc2ddc677c338f73195f31
Alex Neigher <alexneigher@Alexs-MacBook-Pro.local>
2018-09-27 06:59:26 -0700

    user feed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,8 @@ class HomeController < ApplicationController
 
   def show
     dealership = current_user.dealership
-    @grouped_shapes = dealership.shapes.group(:shape_type).count
-    @vehicles = dealership.vehicles.includes(:current_parking_tag)
-    @user_events = Event.includes(:user, {tag: :vehicle}).where(user_id: current_user.dealership.users.pluck(:id)).where(tags: {active: true})
+    
+    @users_with_events = current_user.dealership.users.includes(events: :tag).where(tags: {active: true})
   end
 
   def map_builder
