26caf9c514b12c972f7e6247bc23b37ba57783ae
Alex Neigher <alexneigher@Alexs-MacBook-Pro.local>
2018-09-28 12:56:20 -0600

    fix counts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def show
     dealership = current_user.dealership
-    @users_with_events = current_user.dealership.users.includes(events: :tag).where(tags: {active: true})
+    @users_with_events = current_user.dealership.users.includes(events: :tag)
   end
 
   def map_builder
