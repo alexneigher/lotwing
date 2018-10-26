7f4aa5b4f7ed774085b7f156e46345cceaef4a9d
Alex Neigher <alexneigher@Alexs-MacBook-Pro.local>
2018-10-25 18:23:18 -0700

    adding mtd and last name serch filters

--- app/controllers/board_managers_controller.rb
+++ app/controllers/board_managers_controller.rb
@@ -1,6 +1,18 @@
 class BoardManagersController < ApplicationController
 
   def show
-    @deals = current_user.dealership.deals.all
+    deals = current_user.dealership.deals
+
+    if params.dig(:filters, :mtd).present?
+      deals = deals.where("created_at > ?", Date.today.beginning_of_month)
+    else
+      deals = deals.where("created_at > ?", Date.today.beginning_of_day)
+    end
+
+    if params.dig(:filters, :query).present?
+      deals = deals.where("client_last_name ILIKE ?", "%#{params.dig(:filters, :query)}%")
+    end
+
+    @deals = deals
   end
 end
