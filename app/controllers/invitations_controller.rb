832d60c2eeed064ee9ad737b83261232a141c225
Alex Neigher <alexneigher@Alexs-MacBook-Pro.local>
2018-11-30 17:23:38 -0800

    if you click on the invite again. go home

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -4,6 +4,7 @@ class InvitationsController < ApplicationController
 
   def view_invitation
     @user = User.find_by_reset_password_token(params[:reset_password_token])
+    redirect_to root_path and return unless @user
   end
 
   def accept_invitation
