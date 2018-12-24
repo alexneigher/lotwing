7bf91a040d55c1c3e6fca90a84a7be19738d6c82
Alex Neigher <alexneigher@Alexs-MacBook-Pro.local>
2018-12-24 10:03:01 -0800

    basic crud

--- app/controllers/check_requests_controller.rb
+++ app/controllers/check_requests_controller.rb
@@ -9,6 +9,46 @@ class CheckRequestsController < ApplicationController
   end
 
   def create
+    @check_request = current_user.dealership.check_requests.create(check_request_params)
+
+    if params[:commit] == "Print"
+      redirect_to check_request_printout_path(@check_request, format: :pdf) and return
+    else
+      redirect_to edit_check_request_path(@check_request) and return
+    end
   end
+
+  def update
+    @check_request = current_user.dealership.check_requests.find(params[:id])
+    @check_request.update(check_request_params)
+    if params[:commit] == "Print"
+      redirect_to check_request_printout_path(@check_request, format: :pdf) and return
+    else
+      redirect_to check_requests_path and return
+    end
+  end
+
+  def edit
+    @check_request = current_user.dealership.check_requests.find(params[:id])
+  end
+
+
+  private
+    def check_request_params
+      params.require(:check_request).permit(
+        :is_check,
+        :is_cash,
+        :request_by,
+        :request_date,
+        :department,
+        :amount_requested,
+        :stock_number,
+        :make,
+        :model,
+        :payable_to,
+        :description
+      )
+    end
+
 
 end
