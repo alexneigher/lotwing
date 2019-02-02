9090db77be927a425aa942cf254d020ffe47e512
Alex Neigher <alexneigher@Alexs-MacBook-Pro.local>
2019-02-02 15:49:48 -0800

    adding back in deal counts:

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,7 @@ module ApplicationHelper
     rep_hash = Hash[*reps.map{ |x| [x, {new: 0, used: 0} ] }.flatten]
 
     deals.where(split_rep:'').group_by(&:sales_rep).each do |k , v| 
+
       rep_hash[k] = {
                       new: v.select{|d| d.is_used == false}.count, 
                       used: v.select{|d| d.is_used == true}.count
@@ -13,11 +14,12 @@ module ApplicationHelper
     end
 
     deals.where.not(split_rep:'').each do |d|
+
       rep_hash[d.sales_rep][d.is_used? ? :used : :new] += 0.5
       rep_hash[d.split_rep][d.is_used? ? :used : :new] += 0.5
     end
 
-    return rep_hash
+    return rep_hash.delete_if{|k,v | k.blank?}
 
     #looks like = [{name: {new: count, used: count} }, {name: {new: count, used: count} }]
   end
