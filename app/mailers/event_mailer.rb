88a33cccefba52b7949a112ba9f312b6cee511aa
Alex Neigher <alexneigher@Alexs-MacBook-Pro.local>
2019-03-31 11:09:15 -0700

    more correct multi to mailer

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -1,14 +1,21 @@
 class EventMailer < ActionMailer::Base
   default :from => 'admin@lotwing.herokuapp.com'
 
-  def notify_about_note_created(event_id)
-    @event = Event.find(event_id)
+  def self.notify_about_note_created(event)
+    @event = event
 
     recipients = @event.user.dealership.new_note_notification_addresses.split(',').uniq
     
     recipients.each do |email|
-      mail( to: email, subject: "#{@event.user.full_name} has added a new note." ).deliver
+      EventMailer.new_note_created(event, email.strip).deliver
     end
+  end
+
+  def new_note_created(event, email)
+    @event = event
+    @email = email
+
+    mail( to: @email, subject: "#{@event.user.full_name} has added a new note." )
   end
 
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -7,11 +7,11 @@ class Event < ApplicationRecord
   enum event_type: [:tag, :note, :test_drive, :fuel_vehicle, :odometer_update, :photo_update, :write_up, :mark_sold]
 
 
-  after_create :notify_dealership, if: Proc.new { self.event_type == "note" }
+  after_create :notify_dealership, if: Proc.new { self.event_type == 'note' }
 
 
   private
     def notify_dealership
-      EventMailer.notify_about_note_created(self.id)
+      EventMailer.notify_about_note_created(self)
     end
 end
