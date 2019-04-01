94d4bed19ecae2f59a4b45de76260905281845d0
Alex Neigher <alexneigher@Alexs-MacBook-Pro.local>
2019-04-01 07:40:37 -0700

    adding ack fix

--- app/controllers/web_api/events_controller.rb
+++ app/controllers/web_api/events_controller.rb
@@ -2,7 +2,7 @@ module WebApi
   class EventsController < ApplicationController
     def index
       @note_events = current_user.dealership.events.includes(tag: :shape).where(event_type: "note", acknowledged: :false).where(tags: {active: :true})
-      @test_drive_events = current_user.dealership.events.includes(tag: :shape).where(event_type: "test_drive").where(tags: {active: :true})
+      @test_drive_events = current_user.dealership.events.includes(tag: :shape).where(event_type: "test_drive", acknowledged: :false).where(tags: {active: :true})
       
       render json: {
                       note_events: @note_events.map{|e| EventSerializer.new(e).serialized_json},
