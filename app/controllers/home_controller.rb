0c5f7edf2b93f15c29d5d2ab0f778feaf8be0b0c
Alex Neigher <alexneigher@Alexs-MacBook-Pro.local>
2019-08-18 14:15:41 -0700

    sorting

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,10 @@ class HomeController < ApplicationController
 
   def show
     dealership = current_user.dealership
-    @users_with_events = current_user.dealership.users.includes(events: :tag).where(events: {created_at: [24.hours.ago..DateTime.current]})
+    users_with_events = current_user.dealership.users.includes(events: :tag).where(events: {created_at: [24.hours.ago..DateTime.current]})
     @vehicles_off_lot = current_user.dealership.vehicles.joins(:events).where.not(events: {started_at: nil}).where(events: {ended_at: nil}).where(events: {event_type: ["test_drive", "fuel_vehicle"]})
+    
+    @users_with_events = users_with_events.sort{|a, b| b.events.length <=> a.events.length}
   end
 
   def map_builder
