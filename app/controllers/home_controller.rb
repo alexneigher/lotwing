fabb427f7b2a920a301352ecd9bc5248403c6f36
Alex Neigher <alexneigher@Alexs-MacBook-Pro.local>
2019-08-26 21:39:19 -0700

    in time zone

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   def show
     dealership = current_user.dealership
     users_with_24h_events = current_user.dealership.users.includes(events: :tag).where(events: {created_at: [24.hours.ago..DateTime.current]})
-    users_with_today_events = current_user.dealership.users.includes(events: :tag).where(events: {created_at: [DateTime.current.beginning_of_day..DateTime.current]})
+    users_with_today_events = current_user.dealership.users.includes(events: :tag).where(events: {created_at: [DateTime.current.in_time_zone("Pacific Time (US & Canada)").beginning_of_day..DateTime.current]})
     @vehicles_off_lot = current_user.dealership.vehicles.joins(:events).where.not(events: {started_at: nil}).where(events: {ended_at: nil}).where(events: {event_type: ["test_drive", "fuel_vehicle"]})
     
     @users_with_24h_events = users_with_24h_events.sort{|a, b| b.events.length <=> a.events.length}
