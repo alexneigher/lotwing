078d57ddc3e7aabc3248d3fea7ed8abda5a1cfb5
Alex Neigher <alexneigher@Alexs-MacBook-Pro.local>
2019-09-28 14:48:28 -0700

    differentiate user created

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -2,7 +2,7 @@ class VehiclesController < ApplicationController
   
   def index
     dealership = current_user.dealership
-    all_vehicles = dealership.vehicles.includes(:current_parking_tag, :open_service_tickets)
+    all_vehicles = dealership.vehicles.includes(:current_parking_tag, :open_service_tickets, :events)
     filtered_vehicles = all_vehicles
 
     if params.dig(:filter, :model).present?
