a00b8991a9827dacd901707654bcf2eef98ab96c
Alex Neigher <alexneigher@Alexs-MacBook-Pro.local>
2019-11-06 18:23:56 -0800

    adding sales and service holds to api

--- app/controllers/api/shapes_controller.rb
+++ app/controllers/api/shapes_controller.rb
@@ -25,20 +25,20 @@ module Api
     def parking_spaces
       @parking_spaces = current_user.dealership.shapes.where(shape_type: 'parking_space')
 
-      @new_vehicle_occupied_space = @parking_spaces.joins(:vehicle).where(vehicles: {usage_type: "is_new"})
-      @used_vehicle_occupied_space = @parking_spaces.joins(:vehicle).where(vehicles: {usage_type: "is_used"})
+      @new_vehicle_occupied_space = @parking_spaces.joins(:vehicle).where(vehicles: {usage_type: "is_new", sold_status: nil})
+      @used_vehicle_occupied_space = @parking_spaces.joins(:vehicle).where(vehicles: {usage_type: "is_used", sold_status: nil})
       
-      @loaner_occupied_spaces = @parking_spaces.joins(:vehicle).where(vehicles: {usage_type: "loaner"})
-      @lease_return_occupied_spaces = @parking_spaces.joins(:vehicle).where(vehicles: {usage_type: "lease_return"})
-      @wholesale_unit_occupied_spaces = @parking_spaces.joins(:vehicle).where(vehicles: {usage_type: "wholesale_unit"})
+      @loaner_occupied_spaces = @parking_spaces.joins(:vehicle).where(vehicles: {usage_type: "loaner", sold_status: nil})
+      @lease_return_occupied_spaces = @parking_spaces.joins(:vehicle).where(vehicles: {usage_type: "lease_return", sold_status: nil})
+      @wholesale_unit_occupied_spaces = @parking_spaces.joins(:vehicle).where(vehicles: {usage_type: "wholesale_unit", sold_status: nil})
 
-      @service_hold_spaces = @parking_spaces.joins(:vehicle).where(vehicles: {service_hold: true})
-
+      @service_hold_spaces = @parking_spaces.joins(:vehicle).where(vehicles: {service_hold: true}) #this is superimposed on all other "types"
+      @sales_hold_spaces = @parking_spaces.joins(:vehicle).where(vehicles: {sales_hold: true}) #this is superimposed on all other "types"
       @sold_vehicle_spaces = @parking_spaces.joins(:vehicle).where.not(vehicles: {sold_status: nil})
 
       @duplicate_shape_ids = @parking_spaces.includes(:tags).where(tags: {active: true}).select{|p| p.tags.length > 1}
 
-      all_vehicle_spaces  = [@new_vehicle_occupied_space + @used_vehicle_occupied_space + @loaner_occupied_spaces + @lease_return_occupied_spaces +@wholesale_unit_occupied_spaces + @sold_vehicle_spaces + @service_hold_spaces].flatten
+      all_vehicle_spaces  = [@new_vehicle_occupied_space + @used_vehicle_occupied_space + @loaner_occupied_spaces + @lease_return_occupied_spaces +@wholesale_unit_occupied_spaces + @sold_vehicle_spaces + @service_hold_spaces + @sales_hold_spaces].flatten
       
       @empty_parking_space = @parking_spaces - all_vehicle_spaces
 
@@ -51,8 +51,9 @@ module Api
                     sold_vehicle_spaces: @sold_vehicle_spaces,
                     empty_parking_spaces: @empty_parking_space,
                     duplicate_parked_spaces: @duplicate_shape_ids,
-                    service_hold_spaces: @service_hold_spaces 
-                   }
+                    service_hold_spaces: @service_hold_spaces,
+                    sales_hold_spaces: @sales_hold_spaces
+                  }
     end
 
     def show
