d0d699dddd5e2fecadf696060990998dfacb4be1
Alex Neigher <alexneigher@Alexs-MacBook-Pro.local>
2020-01-16 20:47:06 -0800

    stubbing out counts

--- app/controllers/home/user_activity_controller.rb
+++ app/controllers/home/user_activity_controller.rb
@@ -1,7 +1,11 @@
 class Home::UserActivityController < ApplicationController
 
   def index
-    
+    all_events = current_user.dealership.events
+
+    user_events = all_events.group(:user).count
+
+    @role_grouped_user_events = user_events.group_by{|u, v| u.permission_level}
   end
 
 end
