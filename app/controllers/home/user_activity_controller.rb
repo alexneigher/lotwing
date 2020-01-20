7fb28fb464df48efaee88f9008ecdda15394fa69
Alex Neigher <alexneigher@Alexs-MacBook-Pro.local>
2020-01-19 19:53:58 -0800

    user activity feed

--- app/controllers/home/user_activity_controller.rb
+++ app/controllers/home/user_activity_controller.rb
@@ -1,11 +1,33 @@
 class Home::UserActivityController < ApplicationController
 
   def index
-    all_events = current_user.dealership.events
+    hash = {}
 
-    user_events = all_events.group(:user).count
+    all_dealership_users = current_user.dealership.users
 
-    @role_grouped_user_events = user_events.group_by{|u, v| u.permission_level}
+    grouped_by_user_events = current_user
+                              .dealership
+                              .events
+                              .where("events.created_at >= ?", Date.today.beginning_of_month)
+                              .group_by(&:user_id)
+
+    grouped_by_user_events.each do |user_id, events|
+      last_day_events = events.select{|e| e.created_at >= 1.day.ago.beginning_of_day}.count
+      last_week_events = events.select{ |e| e.created_at >= 1.week.ago.beginning_of_day}.count
+      mtd_events = events.select{|e| e.created_at >= Date.today.beginning_of_month}.count
+
+      user = all_dealership_users.detect{|u| u.id == user_id }
+
+      hash[user] = {"1" => last_day_events, "7" => last_week_events, "mtd" => mtd_events}
+    end
+
+    remaining_users = all_dealership_users - hash.keys
+
+    remaining_users.each do |u|
+      hash[u] = {"1" => 0, "7" => 0, "mtd" => 0}
+    end
+    
+    @role_grouped_user_events = hash.group_by{ |u, event_counts| u.permission_level }
   end
 
 end
