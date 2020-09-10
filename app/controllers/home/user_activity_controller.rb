df3e86139a80318964083bbe5c6ead623b15f979
Alex Neigher <alexneigher@Alexs-MacBook-Pro.local>
2020-09-09 19:46:25 -0700

    adding date filters to activity

--- app/controllers/home/user_activity_controller.rb
+++ app/controllers/home/user_activity_controller.rb
@@ -5,13 +5,25 @@ class Home::UserActivityController < ApplicationController
 
     @event_type = params[:event_type].presence
 
+    @start_date = params[:start_date]
+    @end_date = params[:end_date]
+
     event_type_hash = @event_type ? {event_type: @event_type}:{}
 
     all_dealership_users = current_user.dealership.users.active
 
-    grouped_by_user_events = current_user.dealership.events.where(event_type_hash).where("events.created_at >= ?", DateTime.current.in_time_zone("US/Pacific").beginning_of_month).group_by(&:user_id)
+    if params[:start_date].present?
+      start_date = DateTime.strptime(params.dig(:start_date), "%Y-%m-%d").beginning_of_day
+      end_date =  DateTime.strptime(params.dig(:end_date).presence || Date.today.strftime("%Y-%m-%d"), "%Y-%m-%d").end_of_day
 
-    grouped_by_user_events.each do |user_id, events|
+      sql = "events.created_at >= '#{start_date}' AND events.created_at <= '#{end_date}'"
+    else
+      sql = "events.created_at >= '#{DateTime.current.in_time_zone('US/Pacific').beginning_of_month}'"
+    end
+
+    user_events = current_user.dealership.events.where(event_type_hash).where(sql).group_by(&:user_id)
+
+    user_events.each do |user_id, events|
       next unless user_id.in?(all_dealership_users.pluck(:id))
       last_day_events = events.select{|e| e.created_at.in_time_zone("US/Pacific") >= DateTime.current.in_time_zone("US/Pacific").beginning_of_day}.count
       last_week_events = events.select{ |e| e.created_at.in_time_zone("US/Pacific") >= 1.week.ago.in_time_zone("US/Pacific").beginning_of_day}.count
