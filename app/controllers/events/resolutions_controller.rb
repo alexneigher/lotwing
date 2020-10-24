6e714f0b9ea221e8ea222ac354267de2346d61f6
Alex Neigher <alexneigher@Alexs-MacBook-Pro.local>
2020-10-24 11:54:25 -0700

    when fixing note redirect to root

--- app/controllers/events/resolutions_controller.rb
+++ app/controllers/events/resolutions_controller.rb
@@ -1,20 +1,20 @@
 module Events
   class ResolutionsController < ApplicationController
-    
+
     def new
       @event = current_user.dealership.events.find(params[:event_id])
     end
 
     def create
-      
+
       @event = current_user.dealership.events.find(params[:event_id])
       @resolution = @event.resolutions.create(resolution_params.merge(user_id: current_user.id))
-      
+
       if params[:commit] == 'Save & Resolve'
         @event.update(acknowledged: true)
       end
 
-      redirect_to vehicles_path
+      redirect_to root_path
     end
 
 
