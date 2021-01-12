53180c0639b76f3345a29bfc8fe9e29eefb4d6c1
Alex Neigher <alexneigher@Alexs-MacBook-Pro.local>
2021-01-11 22:14:40 -0800

    solve for nil

--- app/helpers/service_ticket_department_helper.rb
+++ app/helpers/service_ticket_department_helper.rb
@@ -1,7 +1,7 @@
 module ServiceTicketDepartmentHelper
   def service_ticket_department_ownership_text(std, user_method, timestamp_method)
-    str1 = std.send(user_method)&.full_name
-    str2 = std.send(timestamp_method)&.in_time_zone("US/Pacific")&.strftime("%m/%d/%Y %l:%M %p")
+    str1 = std.send(user_method)&.full_name.presence || ""
+    str2 = std.send(timestamp_method)&.in_time_zone("US/Pacific")&.strftime("%m/%d/%Y %l:%M %p").presence || ""
 
     return str1 + "<br>"+ str2
   end
